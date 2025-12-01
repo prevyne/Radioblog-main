d0393a7b260486388ed0636d7749b261d2b847e6
ThedonJim <royouma50@gmail.com>
2025-12-01 22:25:48 +0300

    Update Depndancies

--- client/src/utils/apiCalls.js
+++ client/src/utils/apiCalls.js
@@ -5,7 +5,7 @@ import axios from "axios";
 export const API_BASE_URL = process.env.REACT_APP_API_URL || "/api";
 
 const api = axios.create({
-  baseURL: API_BASE_URL,
+  baseURL: "https://radioblog-mai.onrender.com/api",
   withCredentials: true,
 });
 
